18f64011a925ad0cc376abb7b1fc86d7cd25c92b
John Alexander Solano Ramirez <jsolano@rentingcolombia.com>
2025-11-17 01:22:52 -0500

    Se hacen ajustes en las ventanas, lista manifiesto, y principal

--- src/app/listamanifiesto/listamanifiesto.component.ts
+++ src/app/listamanifiesto/listamanifiesto.component.ts
@@ -12,12 +12,7 @@ export class ListamanifiestoComponent implements OnInit {
   filtroManifiesto: any;
   listaManifiesto: any[] = [];
   manifiestoOriginales: any[] = [];
-  dataGuia = {
-    id_Guia: 0,
-    destinatario: '',
-    ciudadDestino: '',
-    fechaInclusion: '',
-  };
+  listaDetalleManifiesto: any[] = [];
   token: any = '';
 
   constructor(
@@ -40,33 +35,32 @@ export class ListamanifiestoComponent implements OnInit {
   filtrarManifiesto() {
     const filtro = this.filtroManifiesto.toLowerCase();
     this.listaManifiesto = this.manifiestoOriginales.filter(
-      (mani: any) =>
-        mani.nombreCliente.toLowerCase().includes(filtro) ||
-        mani.cuenta.toLowerCase().includes(filtro)
+      (m: any) =>
+        m.nombreCliente.toLowerCase().includes(filtro) ||
+        m.id_Manifiesto.toString().toLocaleLowerCase().includes(filtro)
     );
   }
 
   verDetalleGuia(guia: any) {
-    console.log(guia);
-    this.dataGuia.id_Guia = guia[0].id_Guia;
-    this.dataGuia.destinatario = guia[0].destinatario;
-    this.dataGuia.ciudadDestino = guia[0].ciudadDestino;
-    this.dataGuia.fechaInclusion = guia[0].fechaInclusion;
-    this.validarDatos(this.dataGuia);
+    this.listaDetalleManifiesto = [];
+    this.listaDetalleManifiesto = guia;
+    this.validarDatos(this.listaDetalleManifiesto);
     let modalElement = document.getElementById('detalleModal');
     let modal = new bootstrap.Modal(modalElement);
     modal.show();
-    //alert(JSON.stringify(guia));
   }
   validarDatos(guia: any) {
-    if (guia.destinatario == '' || guia.destinatario == null) {
-      this.dataGuia.destinatario = 'Sin destinatario';
-    }
-    if (guia.ciudadDestino == '' || guia.ciudadDestino == null) {
-      this.dataGuia.ciudadDestino = 'Sin ciudad de destino';
-    }
-    if (guia.fechaInclusion == '' || guia.fechaInclusion == null) {
-      this.dataGuia.fechaInclusion = 'Sin fecha de inclusión';
+    for (let index = 0; index < guia.length; index++) {
+      const element = guia[index];
+      if (element.destinatario == '' || element.destinatario == null) {
+        element.destinatario = 'Sin destinatario';
+      }
+      if (element.ciudadDestino == '' || element.ciudadDestino == null) {
+        element.ciudadDestino = 'Sin ciudad de destino';
+      }
+      if (element.fechaInclusion == '' || element.fechaInclusion == null) {
+        element.fechaInclusion = 'Sin fecha de inclusión';
+      }
     }
   }
 }
